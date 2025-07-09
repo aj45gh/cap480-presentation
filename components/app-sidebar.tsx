41d43488ee2c568f8b80cebcf9bd43512cb2ae1b
Alex James <alexjames12345@hotmail.com>
2025-07-08 22:02:36 -0400

    basic nav sidebar

---
+++ components/app-sidebar.tsx
@@ -0,0 +1,58 @@
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "@/components/ui/sidebar";
+
+const items = [
+  {
+    title: "Home",
+    url: "#",
+  },
+  {
+    title: "Problem",
+    url: "#",
+  },
+  {
+    title: "Solution",
+    url: "#",
+  },
+  {
+    title: "Data",
+    url: "#",
+  },
+  {
+    title: "Considerations",
+    url: "#",
+  },
+];
+
+export function AppSidebar() {
+  return (
+    <Sidebar>
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarGroupLabel>Pages</SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {items.map((item) => (
+                <SidebarMenuItem key={item.title}>
+                  <SidebarMenuButton asChild>
+                    <a href={item.url}>
+                      <span>{item.title}</span>
+                    </a>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
+    </Sidebar>
+  );
+}
