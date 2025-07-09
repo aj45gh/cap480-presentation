33ae3556bd02248c8d258d829e37f78bc827381f
Alex James <alexjames12345@hotmail.com>
2025-07-08 22:59:22 -0400

    survey data so far

---
+++ components/chart.tsx
@@ -0,0 +1,39 @@
+const results = [
+  {
+    experience: "passive interest",
+    uses: [
+      "primary output",
+      "secondary output",
+      "explain concepts",
+      "brainstorming",
+    ],
+    score: 10,
+  },
+  { experience: "passive interest", uses: ["explain concepts"], score: 8 },
+  { experience: "passive interest", uses: ["brainstorming"], score: 10 },
+  {
+    experience: "senior level",
+    uses: [
+      "primary output",
+      "secondary output",
+      "explain concepts",
+      "brainstorming",
+    ],
+    score: 9,
+  },
+  {
+    experience: "passive interest",
+    uses: ["primary output", "explain concepts", "brainstorming"],
+    score: 7,
+  },
+  {
+    experience: "senior level",
+    uses: [
+      "primary output",
+      "secondary output",
+      "explain concepts",
+      "brainstorming",
+    ],
+    score: 10,
+  },
+];
