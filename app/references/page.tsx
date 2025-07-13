5e220c2bae20966f21fa9b3726238934d0ff4c0a
Alex James <alexjames12345@hotmail.com>
2025-07-13 11:17:10 -0400

    added base references page

---
+++ app/references/page.tsx
@@ -0,0 +1,11 @@
+import { Conisoft, Edtech, Chatbots } from "@/components/references";
+
+export default function Page() {
+  return (
+    <div className="flex flex-col">
+      <Conisoft />
+      <Edtech />
+      <Chatbots />
+    </div>
+  );
+}
---
+++ components/references.tsx
@@ -0,0 +1,47 @@
+export function Conisoft() {
+  return (
+    <div className="-indent-8">
+      Tona, C., Juárez-Ramírez, R., Jiménez, S., & Durán, M. (2024). Exploring
+      LLM tools through the eyes of industry experts and novice programmers.{" "}
+      <span className="italic">
+        Proceedings of the International Conference in Software Engineering
+        Research and Innovation (CONISOFT), 12,&nbsp;
+      </span>
+      313-321&nbsp;
+      <a href="https://doi.org/10.1109/CONISOFT63288.2024.00048">
+        https://doi.org/10.1109/CONISOFT63288.2024.00048
+      </a>
+    </div>
+  );
+}
+
+export function Edtech() {
+  return (
+    <div className="-indent-8">
+      Huang, H. (2024). Promoting students' creative and design thinking with
+      generative AI-supported co-regulated learning: Evidence from digital game
+      development projects in healthcare courses.
+      <span className="italic">Educational Technology & Society, 27</span>(4),
+      487+
+      <a href="http://dx.doi.org.revproxy.ecpi.edu/10.30191/ETS.202410_27(4).TP04">
+        http://dx.doi.org.revproxy.ecpi.edu/10.30191/ETS.202410_27(4).TP04
+      </a>
+    </div>
+  );
+}
+
+export function Chatbots() {
+  return (
+    <div className="-indent-8">
+      Davar, N. F., Dewan, M. A. A., & Zhang, X. (2025). AI Chatbots in
+      Education: Challenges and Opportunities.
+      <span className="italic">
+        Information<span className="not-italic">[Basel],</span>16
+      </span>
+      (3).
+      <a href="http://dx.doi.org.revproxy.ecpi.edu/10.3390/info16030235">
+        http://dx.doi.org.revproxy.ecpi.edu/10.3390/info16030235
+      </a>
+    </div>
+  );
+}
