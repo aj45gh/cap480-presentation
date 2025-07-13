a82370bde0472e5d21122a5c5494c2f5bd355553
Alex James <alexjames12345@hotmail.com>
2025-07-13 17:32:06 -0400

    basic chart setup

--- app/data/page.tsx
+++ app/data/page.tsx
@@ -1,9 +1,10 @@
 import { ContentWithSidebar } from "@/components/app-sidebar";
+import { FirstPie } from "@/components/chart";
 
-export default function Home() {
+export default function Page() {
   return (
     <ContentWithSidebar headerText="Placeholder header">
-      <></>
+      <FirstPie />
     </ContentWithSidebar>
   );
 }
